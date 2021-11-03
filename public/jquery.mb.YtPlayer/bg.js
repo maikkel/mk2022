5e754586333b9c0b82b4dcb08831fb7cf6295f02
maikk <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>
2021-11-03 19:39:37 +0100

    Update

--- public/jquery.mb.YtPlayer/bg.js
+++ public/jquery.mb.YtPlayer/bg.js
@@ -1,19 +1,19 @@
 $(function() {
-    // $("#bgndVideo").YTPlayer({
-    //     videoURL: 'dPfWA5kllvU',
-    //     containment: 'body',
-    //     autoPlay: true,
-    //     mute: true,
-    //     // startAt: Math.floor(Math.random() * 900),
-    //     startAt: 0,
-    //     opacity: 1,
-    //     showControls: false,
-    //     quality: 'highres',
-    //     loop: true,
-    //     showYTLogo: true,
-    //     addRaster: true,
-    //     stopMovieOnBlur: false,
-    //     ratio: '16/9',
-    //     optimizeDisplay: true
-    // });
+    $("#bgndVideo").YTPlayer({
+        videoURL: 'dPfWA5kllvU',
+        containment: 'body',
+        autoPlay: true,
+        mute: true,
+        // startAt: Math.floor(Math.random() * 900),
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        quality: 'highres',
+        loop: true,
+        showYTLogo: true,
+        addRaster: true,
+        stopMovieOnBlur: false,
+        ratio: '16/9',
+        optimizeDisplay: true
+    });
 });
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import MenuItem from "./MenuItem.js";
 
 export default function Menu({ menuData }) {
   const menuItems = menuData.map((item, i) => {
-    return <MenuItem itemData={item} key={item.key} index={i}/>;
+    return <MenuItem itemData={item} key={item.key} index={i} total={menuData.length}/>;
   });
 
   return (
