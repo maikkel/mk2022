c4475485a7177f4a3d1ba375f80ba9143fc51916
makell <maik.kellerahls@comfone.com>
2021-11-03 17:03:13 +0100

    Menu

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,11 @@
 import "./MenuItem.scss";
 import { Link } from "react-router-dom";
 
-export default function MenuItem({ itemData }) {
+export default function MenuItem({ itemData, index }) {
+    const rotation = index * 72;
   return (
-    <li className="menu-item">
-      <Link to={itemData.route}>{itemData.name}</Link>
-    </li>
+      <div className="menu-item" style={{transform: "translate(-50%,-50%) rotate("+ rotation +"deg)"}}>
+        <Link className="link" to={itemData.route} style={{transform: "rotate(-"+ rotation +"deg)"}}>{itemData.name}</Link>
+      </div>
   );
 }
