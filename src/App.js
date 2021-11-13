24e445439d8e12eac5a44b314b751f638873990b
maikk <maik.kellerhals@gmail.com>
2021-11-13 23:26:50 +0100

    server side data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import "./scss/App.reset.scss";
 import "./scss/App.utils.scss";
 import "./scss/App.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Menu from "./components/Menu";
 import Content from "./components/Content";
-import menuData from "./data/menuData";
 
 export default function App() {
-  return (
-    <Router>
-      <div className="app">
-        <div className="container center">
-          <Menu menuData={menuData} />
-          <Content menuData={menuData} />
+  const [menuData, setMenuData] = useState(null);
+  const [loadingText, setLoadingText] = useState("");
+
+  useEffect(() => {
+    console.log("ue");
+    fetch("https://api.maikkellerhals.com/menu-items")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setMenuData(result);
+          console.info("menuData loaded");
+          console.log(result);
+        },
+        (error) => {
+          setLoadingText("LOADING ERROR!");
+          console.error("ERROR while loading menuData -", error);
+        }
+      );
+  }, []);
+
+  if (menuData) {
+    return (
+      <Router>
+        <div className="app">
+          <div className="container center">
+            <Menu menuData={menuData} />
+            <Content menuData={menuData} />
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  } else {
+    return <div className="container center loadingText">{loadingText}</div>;
+  }
 }
